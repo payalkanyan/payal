1574a9a7f78331d8955e5e71fbd9552bf6dd0a57
payalkanyan <youcancallmepayal@gmail.com>
2025-12-27 15:09:09 +0530

    final changes

--- script.js
+++ script.js
@@ -7,3 +7,32 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+
+window.addEventListener("DOMContentLoaded", function() {
+    const form = document.querySelector('form[action^="https://formspree.io/"]');
+    form.addEventListener("submit", function(e) {
+        e.preventDefault();
+        const data = new FormData(form);
+        fetch(form.action, {
+            method: form.method,
+            body: data,
+            headers: {
+                'Accept': 'application/json'
+            }
+        }).then(response => {
+            if (response.ok) {
+                form.reset();
+            } else {
+                response.json().then(data => {
+                    if (Object.hasOwn(data, 'errors')) {
+                        alert(data["errors"].map(error => error["message"]).join(", "))
+                    } else {
+                        alert("Oops! There was a problem submitting your form")
+                    }
+                })
+            }
+        }).catch(error => {
+            alert("Oops! There was a problem submitting your form")
+        });
+    });
+});
